e62fc4e62461317c4bb8099f908136fd9176720e
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-25 10:00:06 -0300

    feat: Implement authentication using Next Auth with Github Provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,30 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
       >
       <FaGithub color="#04d361" />
-      Claudio Orlandi
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
       >
       <FaGithub color="#eba417" />
       Sign In with Github
