09f0dd125adeccc93c819bfa50aa4a35f8596366
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-25 09:05:22 -0300

    chore: Change getServerSideProps to getStaticProps for render static Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IYjk1HbZ41CbdiTXHvhiuLZ');
 
   const product = {
@@ -50,6 +50,9 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
 
   return {
-    props: { product },
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
