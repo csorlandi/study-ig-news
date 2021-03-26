d1ef0335859d76e5c8d3c9a5fce7f82b6a293ae4
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-26 01:40:29 -0300

    chore: Use the Webhook event response to save user subscription on the Database

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -42,7 +43,24 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('evento recebido', event);
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+            );
+
+            break;
+          default:
+            throw new Error('Unhandled event.');
+        }
+      } catch (err) {
+        return response.json({ error: 'Webhook handler failed.' });
+      }
     }
 
     response.json({ received: true });
