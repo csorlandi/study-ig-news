bfce691812493da41a0759a4f0271a548d408c75
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-25 22:18:26 -0300

    feat: Implement FaunaDB Query to persist the logged user in the Database and prevent duplicated users store

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -8,8 +8,6 @@ import styles from './styles.module.scss';
 export function SignInButton() {
   const [session] = useSession();
 
-  console.log(session);
-
   return session ? (
     <button
       type="button"
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -10,4 +13,38 @@ export default NextAuth({
       scope: 'read-user',
     }),
   ],
+  callbacks: {
+    async signIn(user) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }},
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 })
